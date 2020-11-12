beb516f6cbfe4d0097d97b67dc3328674a94953f
Chris <chrisfontas724@gmail.com>
2020-11-11 21:18:12 -0800

    add extension

--- main.cpp
+++ main.cpp
@@ -202,7 +202,9 @@ int main(int argc, char** argv) {
     vk::SurfaceKHR surface = window->createVKSurface(instance->vk());
 
     // Create device extension list.
-    auto device_extensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};//, VK_NV_RAY_TRACING_EXTENSION_NAME};
+    auto device_extensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+                              VK_KHR_GET_MEMORY_REQUIREMENTS_2_EXTENSION_NAME};
+                         //     VK_NV_RAY_TRACING_EXTENSION_NAME};
 
     // Pick the best device given the provided surface.
     auto physical_device = instance->pickBestDevice(surface, device_extensions);
