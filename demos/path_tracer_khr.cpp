27e8bec236c83cd3e81e347311e2feec056f1b0c
Chris Fontas <chrisfontas724@gmail.com>
2023-08-26 13:28:49 -0700

    add khr shaders

--- demos/path_tracer_khr.cpp
+++ demos/path_tracer_khr.cpp
@@ -9,19 +9,20 @@
 
 void PathTracerKHR::setup(gfx::LogicalDevicePtr logical_device, int32_t num_swap, int32_t width, int32_t height) {
 
-    gfx::Geometry geometry; 
-    for (uint32_t i = 0; i < gfx::VTX_ATTR_COUNT; i++) {
-        geometry.attributes[i] = gfx::ComputeBuffer::createStorageBuffer(logical_device, 
-                                                                        sizeof(float)*4*3,
-                                                                        vk::BufferUsageFlagBits::eShaderDeviceAddress);
-    }
-//     geometry.indices = gfx::ComputeBuffer::createStorageBuffer(logical_device, sizeof(uint32_t)*3);
-//     geometry.flags = 0xFFFFFFFF;
-//     geometry.num_indices = 3;
-//     geometry.num_vertices = 3;
+    std::vector<float> positions = {552.8,   0.0,   0.0, 1.0,
+                                    549.6,   0.0, 559.2, 1.0,
+                                    556.0, 548.8, 559.2, 1.0,
+                                    556.0, 548.8,   0.0, 1.0};
+    std::vector<uint32_t> indices = {0,1,2,0,2,3};                                
 
-//     auto as = std::make_shared<gfx::AccelerationStructure>(logical_device, geometry);
-//     CXL_DCHECK(as);
+    gfx::Geometry geometry; 
+    geometry.attributes[gfx::VTX_POS_FLAG] = gfx::ComputeBuffer::createFromVector(logical_device, positions, vk::BufferUsageFlagBits::eShaderDeviceAddress);
+    geometry.indices = gfx::ComputeBuffer::createFromVector(logical_device, indices,  vk::BufferUsageFlagBits::eShaderDeviceAddress);
+    geometry.flags = gfx::VTX_POS_FLAG;
+    geometry.num_indices = 6;
+    geometry.num_vertices = 4;
+    auto as = std::make_shared<gfx::AccelerationStructure>(logical_device, geometry);
+    CXL_DCHECK(as);
 }
 
 void PathTracerKHR::resize(uint32_t width, uint32_t height) {
